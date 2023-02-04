056e2c0e114af1d0b6321788f9dd6c44f1aa2e8e
lola martinhao <lola.martinhao@gmail.com>
2023-02-03 22:08:16 -0300

    working on coffee card

--- src/pages/Home/components/Products/index.tsx
+++ src/pages/Home/components/Products/index.tsx
@@ -1,3 +1,41 @@
+import { ShoppingCart } from 'phosphor-react'
+import {
+  CardContainer,
+  ItemValueContainer,
+  PriceContainer,
+  ProductsContainer,
+  TagContainer,
+  TextContainer,
+} from './styles'
+import Americano from '../../../../assets/coffees/Type=Americano.svg'
+
 export function Products() {
-  return <h1>Products</h1>
+  return (
+    <ProductsContainer>
+      <h2>Nossos cafés</h2>
+
+      <CardContainer>
+        <img src={Americano} alt="Café tipo americano" />
+
+        <TagContainer>
+          <span>Tradicional</span>
+        </TagContainer>
+        <TextContainer>
+          <h3>Expresso Tradicional</h3>
+          <span>O tradicional café feito com água quente e grãos moídos</span>
+          <ItemValueContainer>
+            <PriceContainer>
+              <span>R$</span>
+              <h3>9,90</h3>
+            </PriceContainer>
+
+            <div>
+              <span>Quantity</span>
+              <ShoppingCart weight="fill" />
+            </div>
+          </ItemValueContainer>
+        </TextContainer>
+      </CardContainer>
+    </ProductsContainer>
+  )
 }
---
+++ src/pages/Home/components/Products/styles.ts
@@ -0,0 +1,130 @@
+import styled from 'styled-components'
+
+export const ProductsContainer = styled.div`
+  margin: 0 10rem 10rem;
+
+  h2 {
+    font-weight: 800;
+    font-size: 2rem;
+    line-height: 130%;
+
+    display: flex;
+    align-items: center;
+
+    color: ${(props) => props.theme['gray-800']};
+
+    margin-bottom: 3.375rem;
+  }
+`
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  border-radius: 6px 36px;
+
+  width: 16rem;
+  height: 19.375rem;
+  background: ${(props) => props.theme['gray-200']};
+
+  img {
+    height: 7.5rem;
+    width: 7.5rem;
+    margin-top: -10%;
+  }
+`
+
+export const TextContainer = styled.span`
+  padding: 0 1.25rem;
+  text-align: center;
+
+  h3 {
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.25rem;
+    line-height: 130%;
+    text-align: center;
+
+    margin-bottom: 0.5rem;
+
+    color: ${(props) => props.theme['gray-800']};
+  }
+
+  span {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 130%;
+
+    text-align: center;
+
+    color: ${(props) => props.theme['gray-600']};
+  }
+`
+
+export const TagContainer = styled.span`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  margin: 1rem;
+
+  background: ${(props) => props.theme['yellow-100']};
+  border-radius: 100px;
+
+  span {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 0.625rem;
+    line-height: 130%;
+    padding: 4px 8px;
+
+    text-transform: uppercase;
+
+    color: ${(props) => props.theme['yellow-700']};
+  }
+`
+
+export const ItemValueContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+
+  gap: 1.438rem;
+  margin-top: 2.063rem;
+`
+
+export const PriceContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+
+  gap: 0.2rem;
+
+  span {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 130%;
+
+    color: #574f4d;
+  }
+
+  h3 {
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 1.5rem;
+    line-height: 130%;
+
+    color: #574f4d;
+  }
+`
